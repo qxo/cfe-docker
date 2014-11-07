184181e35678c9ffb230c3e64f81161ec4c6b7db
qxo <qxodream@gmail.com>
2014-11-07 14:13:56 +0800

    support daemon proccess name different with startup script file name

--- cfengine/bin/docker_processes_run.sh
+++ cfengine/bin/docker_processes_run.sh
@@ -8,11 +8,28 @@
 # The commands' basename should become a process in order to be detected by CFEngine.
 # The commands will be run as root in a shell.
 
+#clean history
+sed -i  -n '/^[^#]*$/{N;d};p' /var/cfengine/inputs/processes_run.csv
+# if the command "cmd" not a daemon (just a script startup other deamon process)
+# we should identitify by the daemon process like:
+# 1. append process name with '#' eg: /startOracle.sh#tnslsnr
+# 2. on script add daemon process identitify line ,eg '#cfengine-monitor-proccess:tnslsnr'
 for cmd in "$@"
 do
-  file=${cmd%% *}
-  base=${file##*/}
-  echo "$base, $cmd" >> /var/cfengine/inputs/processes_run.csv
+  pn=$(echo "$cmd" | awk -F'#' '{print $2;}');
+  if [ -z "$pn" ] ; then
+    file=${cmd%% *}
+    pn=${file##*/}
+    if [ -f $file ] ; then
+      mp=$(grep '^#cfengine-monitor-process:.*$' $file | awk -F':' '{print $2}');
+      if [ -n "$mp" ] ; then
+        pn="$mp"
+      fi
+    fi
+  else
+    cmd=$(echo "$cmd" | awk -F'#' '{print $1;}');
+  fi
+  echo "$pn, $cmd" >> /var/cfengine/inputs/processes_run.csv
 done
 
 /var/cfengine/bin/cf-agent
